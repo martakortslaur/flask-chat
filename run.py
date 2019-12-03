16facf594f9aae7233052619e918c66d8d13a5a5
martakortslaur <martakortslaur@gmail.com>
2019-12-03 16:19:29 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,13 @@
+#To have access to the environment variables
+import os 
+from flask import Flask
+#To initalize our new Flaks application
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello There</h1>"
+
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
